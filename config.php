54bcd97103203eaddd95124a8658b2c855374534
Nehemiah Cheburet <cheburet.nehemiah@intsoftkenya.co.ke>
2019-06-24 17:22:10 +0300

    Initial set up

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+ $host='localhost';
+ $username= 'root';
+ $pasword = "";
+ $dbname= "mpesa";
+ $conn= mysqli_connect($host,$username,$pasword,$dbname);
+ if(!$conn){
+     echo "Error in database connection";
+ }
+?>
